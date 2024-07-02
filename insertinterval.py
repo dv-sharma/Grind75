caf644502ce15045522e37ec640473946a356319
dv-sharma <65087388+dv-sharma@users.noreply.github.com>
2024-07-02 13:03:21 -0400

    Solving python problems for Grind75 challenge

---
+++ insertinterval.py
@@ -0,0 +1,17 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+
+        newarr=[]
+
+        for i in range(len(intervals)):
+            if newInterval[1]<intervals[i][0]:
+                newarr.append(newInterval)
+                return newarr + intervals[i:]
+            elif newInterval[0]>intervals[i][1]:
+                newarr.append(intervals[i])
+            else:
+                newInterval=[min(newInterval[0],intervals[i][0]),max(newInterval[1],intervals[i][1])]
+            
+        newarr.append(newInterval)
+        return newarr
+        
