44263ae38ba74d124327e713df42fa9afe87e2f8
dv-sharma <65087388+dv-sharma@users.noreply.github.com>
2024-07-21 15:19:02 -0400

    Coded RestorIPBacktrack.py

---
+++ RestorIPBacktrack.py
@@ -0,0 +1,32 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+
+        if not 4<=len(s)<=12:
+            return []
+
+        def helper(idx,curr):
+
+            #terminal condition
+
+            if len(curr)==4:
+                if idx==len(s):
+                    result.append('.'.join(curr))
+                return
+
+            #recursion condition
+            num=''
+            for i in range(idx,min(idx+3,len(s))):
+                num+=s[i]
+
+                if num[0]== '0' and len(num)>1:
+                    break
+                if 0<= int(num)<=255:
+                    curr.append(num)
+                    helper(i+1,curr)
+                    curr.pop() 
+
+        result=[]
+        helper(0,[])
+        return result
+             
+        
