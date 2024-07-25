606acdfc6b3bc243f424ce7ad47afe9ee3523fb8
dv-sharma <65087388+dv-sharma@users.noreply.github.com>
2024-07-25 11:46:53 -0400

    Create ReversestringIterative.py

---
+++ ReversestringIterative.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        currentNode=head
+        previous=None
+
+        while currentNode:
+            next_tmp_node=currentNode.next
+            currentNode.next=previous
+            previous=currentNode
+            currentNode=next_tmp_node
+
+        return previous
+            
+'''
+
+PREVIOUS    CURRENT->  CURRENT.NEXT - NONE(TAIL)
+Store the next in temp,
+Current.next will point to previous
+Previous will move forward to current node
+and current takes the next temp value to mantain the iteration
+
+'''
+
+
+
+
+
+
+        
